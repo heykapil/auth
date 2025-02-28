31acfa326bb547a6d765c12a58c631cc63b3d45c
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2025-02-28 23:50:50 +0530

    fix-redirect-passkey

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -293,8 +293,8 @@ export function LoginForm() {
     </CardContent>
     </Card>
     <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
-      By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-      and <a href="#">Privacy Policy</a>.
+      By clicking continue, you agree to our <a href="//kapil.app/terms">Terms of Service</a>{" "}
+      and <a href="//kapil.app/privacy">Privacy Policy</a>.
     </div>
     </>
   )
