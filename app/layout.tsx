c0e645f57afa7ee4ac4f13f45f7bc2f3c23ec371
heykapil <kapilchaudhary@gujaratuniversity.ac.in>
2025-11-11 16:35:13 +0530

    Changed the font to google font.

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,14 +2,18 @@ import Header from "@/components/nav/navbar";
 import { SessionSync } from "@/components/nav/session-refetch";
 import { WrapperWithQuery } from "@/components/wrapper";
 import type { Metadata } from "next";
+import { Linden_Hill } from "next/font/google";
 import { Suspense } from "react";
 import { Toaster } from "sonner";
 import "./globals.css";
-
 export const metadata: Metadata = {
   title: "Authentication",
   description: "Auth - Kapil Chaudhary",
 };
+const font = Linden_Hill({
+  weight: ["400"],
+  subsets: ["latin", "latin-ext"],
+});
 
 export default async function RootLayout({
   children,
@@ -18,7 +22,7 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body>
+      <body className={font.className}>
         <Suspense>
           <Header />
           <SessionSync />
